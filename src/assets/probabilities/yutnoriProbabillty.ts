a26f786d0466802dead0352f5120c33e9b6e6e51
daehyuh <rkdeown10@naver.com>
2025-01-25 13:56:04 +0900

    change : 2500 수정

--- src/assets/probabilities/yutnoriProbabillty.ts
+++ src/assets/probabilities/yutnoriProbabillty.ts
@@ -1,6 +1,6 @@
 import json150 from './2025윷놀이/150.json';
 import json500 from './2025윷놀이/750.json';
-import json2500R from './2025윷놀이/750.json';
+import json2500R from './2025윷놀이/2500R.json';
 import jsonLegend from './2025윷놀이/Legend.json';
 import GradeProbability from "@/constant/GradeProbability";
 import JsonProbability from "@/constant/JsonProbability";

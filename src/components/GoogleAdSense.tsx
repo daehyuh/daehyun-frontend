4d3e85138992a6ea1c3de70d9fc9fcde25a61d15
daehyuh <rkdeown10@naver.com>
2025-02-03 07:43:03 +0900

    add : side ad

--- src/components/GoogleAdSense.tsx
+++ src/components/GoogleAdSense.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const AdContainer = styled.div`
   display: none; // 기본적으로 숨김
 
-  @media (min-width: 1000px) {
-    display: block; // 1000px 이상일 때 보이도록 설정
+  @media (min-width: 800px) {
+    display: block; // 800px 이상일 때 보이도록 설정
     width: 100%; // 광고의 너비 설정
     height: 100vh; // 광고의 높이 설정
     background-color: #242426; // 광고 배경색

ce1cce632733f2b3fa6b42c99200c891ca78d4a9
daehyuh <rkdeown10@naver.com>
2025-02-08 18:29:40 +0900

    change : api url

--- src/apis/base/fetchAPI.ts
+++ src/apis/base/fetchAPI.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://152.67.211.81//api"
+const BASE_URL = "http://152.67.211.81:8000/api"
 const generateUrl = (path: string) => `${BASE_URL}/${path}`
 
 const fetchAPI = async <T>(path: string): Promise<T> => {

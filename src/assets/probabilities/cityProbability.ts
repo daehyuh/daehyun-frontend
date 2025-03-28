24216adb727c2f959d53b0e34d3d810fd51b0f1b
daehyuh <rkdeown10@naver.com>
2025-03-28 18:50:46 +0900

    fix 42city

--- src/assets/probabilities/cityProbability.ts
+++ src/assets/probabilities/cityProbability.ts
@@ -1,5 +1,5 @@
 import json150 from './202542시티/150.json';
-import json500 from './202542시티/750.json';
+import json750 from './202542시티/750.json';
 import json2500R from './202542시티/2500R.json';
 import jsonLegend from './202542시티/Legend.json';
 import GradeProbability from "@/constant/GradeProbability";
@@ -8,7 +8,7 @@ import JsonProbability from "@/constant/JsonProbability";
 
 const cityProbability: GradeProbability = {
     '150': json150 as JsonProbability,
-    '750': json500 as JsonProbability,
+    '750': json750 as JsonProbability,
     '2500R': json2500R as JsonProbability,
     'Legend': jsonLegend as JsonProbability
 }

7acaf7e8290099ce824826257718bd8fd61947d8
rkdeown10 <rkdeown10@naver.com>
2025-05-29 17:56:07 +0900

    2500R fix

--- src/assets/probabilities/ballFlickingProbability.ts
+++ src/assets/probabilities/ballFlickingProbability.ts
@@ -1,6 +1,6 @@
+import json2500R from './2025알까기/2500R.json';
 import json150 from './2025알까기/150.json';
-import json500 from './2025알까기/750.json';
-import json2500R from './2025알까기/750.json';
+import json750 from './2025알까기/750.json';
 import jsonLegend from './2025알까기/Legend.json';
 import GradeProbability from "@/constant/GradeProbability";
 import JsonProbability from "@/constant/JsonProbability";
@@ -8,7 +8,7 @@ import JsonProbability from "@/constant/JsonProbability";
 
 const ballFlickingProbability: GradeProbability = {
     '150': json150 as JsonProbability,
-    '750': json500 as JsonProbability,
+    '750': json750 as JsonProbability,
     '2500R': json2500R as JsonProbability,
     'Legend': jsonLegend as JsonProbability
 }
--- src/assets/probabilities/snowballFightProbability.ts
+++ src/assets/probabilities/snowballFightProbability.ts
@@ -1,6 +1,6 @@
+import json2500R from './2024눈싸움/2500R.json';
 import json150 from './2024눈싸움/150.json';
-import json500 from './2024눈싸움/750.json';
-import json2500R from './2024눈싸움/750.json';
+import json750 from './2024눈싸움/750.json';
 import jsonLegend from './2024눈싸움/Legend.json';
 import GradeProbability from "@/constant/GradeProbability";
 import JsonProbability from "@/constant/JsonProbability";
@@ -8,7 +8,7 @@ import JsonProbability from "@/constant/JsonProbability";
 
 const snowballFightProbability: GradeProbability = {
     '150': json150 as JsonProbability,
-    '750': json500 as JsonProbability,
+    '750': json750 as JsonProbability,
     '2500R': json2500R as JsonProbability,
     'Legend': jsonLegend as JsonProbability
 }
